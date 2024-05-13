c54f04c567763502841d43a30d7dc2ad4f187c23
minjnlgc <kunyuquemian@outlook.com>
2024-05-13 23:23:18 +0100

    add build generated by frontend pieces, and display it

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 app.use(cors());
 
+app.use(express.static('dist'))
+
 morgan.token('content', (request, response) => {
     if (!request.body) {
         return;
