6ba32c1bdab82fc69b4ef9717c7a0b9f1c4c3408
minjnlgc <kunyuquemian@outlook.com>
2024-05-14 17:49:29 +0100

    add validation

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ const requestLogger = (request, response, next) => {
 };
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+  console.error(error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' });
-    }
-}
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 
 app.use(cors());
 app.use(express.json());
@@ -51,56 +55,51 @@ app.get("/api/notes/:id", (request, response, next) => {
       }
     })
     .catch((error) => {
-      next(error)
+      next(error);
     });
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-        if (result) {
-            response.status(204).end()
-        } else {
-            response.status(404).end()
-        }
-        
+    .then((result) => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).end();
+      }
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(404).json({
-      error: "content missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
-app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body;
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true} )
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
-})
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
