b3eac4d62786ae80d71127080eab4545c01b758c
minjnlgc <kunyuquemian@outlook.com>
2024-05-14 15:19:32 +0100

    finished part3c 3.13-3.14

--- index.js
+++ index.js
@@ -1,108 +1,113 @@
 const express = require("express");
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const Person = require("./models/person");
 
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :content"
+  )
+);
 app.use(cors());
+app.use(express.static("dist"));
 
-app.use(express.static('dist'))
-
-morgan.token('content', (request, response) => {
-    if (!request.body) {
-        return;
-    }
-    return JSON.stringify(request.body)
-})
+morgan.token("content", (request, response) => {
+  if (!request.body) {
+    return;
+  }
+  return JSON.stringify(request.body);
+});
 
 let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
 ];
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1000);
-}
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((result) => {
+    console.log(result);
+    response.json(result);
+  });
+});
 
-app.get('/api/persons', (request, response) => {
-    response.send(persons);
-})
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const message = `<p>Phonebook has info for ${
+    persons.length
+  } people</p><p>${date.toString()}</p>`;
+  response.send(message);
+});
 
-app.get('/info', (request, response) => {
-    const date = new Date();
-    const message = `<p>Phonebook has info for ${persons.length} people</p><p>${date.toString()}</p>`
-    response.send(message)
-})
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id);
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
 
-    if (!person) {
-        response.status(404).end();
-    }
-    response.send(person);
-})
+  response.status(204).end();
+});
 
-app.delete('/api/persons/:id', (request, response) => {
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-    const id = Number(request.params.id);
-    persons = persons.filter(p => p.id !== id);
+  console.log("post request");
+  console.log("post body:", body);
 
-    response.status(204).end()
-})
+  if (!body.name | !body.number) {
+    console.log("missing content");
+    return response
+      .status(400)
+      .json({
+        error: "missing name or number",
+      })
+      .end();
+  } else if (persons.find((p) => p.name === body.name)) {
+    console.log("duplicate name");
+    return response
+      .status(400)
+      .json({
+        error: "name must be unique",
+      })
+      .end();
+  }
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-    console.log('post request')
-    console.log('post body:', body)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
 
-    if (!body.name | !body.number) {
-        console.log('missing content')
-        return response.status(400).json({
-            error: 'missing name or number'
-        }).end();
-
-    } else if (persons.find(p => p.name === body.name)) {
-        console.log('duplicate name');
-        return response.status(400).json({
-            error: 'name must be unique'
-        }).end();
-    }
-
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person)
-
-    response.json(person)
-})
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
