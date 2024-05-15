5315bdbe03a4fc4d62e548d34177972b29760c54
minjnlgc <kunyuquemian@outlook.com>
2024-05-15 21:34:55 +0100

    part4 a

--- models/note.js
+++ models/note.js
@@ -1,20 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URL;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((result) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
